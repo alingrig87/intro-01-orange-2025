4fe013e7d7e4e1be270bbed7e4160600ab6552a9
Alin Grigorescu <alin.grig87@gmail.com>
2025-06-04 20:40:52 +0300

    IMC Calculator

--- JavaScript/Calculator Indice Masa Corporala/app.js
+++ JavaScript/Calculator Indice Masa Corporala/app.js
@@ -1,1 +1,33 @@
-alert('Pagina s-a incarcat!');
+function calculeazaIMC() {
+	var inaltimeInput = document.getElementById('inaltime');
+	var inaltime = Number(inaltimeInput.value);
+
+	var greutateInput = document.getElementById('greutate');
+	var greutate = Number(greutateInput.value);
+
+	var imc = (greutate / (inaltime * inaltime)).toFixed(2);
+
+	var mesajDeOutput;
+	var color;
+
+	if (imc < 18.5) {
+		mesajDeOutput = 'IMC: ' + imc + ' Subponderal';
+		color = 'blue';
+	} else if (imc >= 18.5 && imc <= 25) {
+		mesajDeOutput = 'IMC: ' + imc + ' Greutate normala';
+		color = 'green';
+	} else if (imc > 25 && imc < 30) {
+		mesajDeOutput = 'IMC: ' + imc + ' Supraponderal';
+		color = 'orange';
+	} else {
+		mesajDeOutput = 'IMC: ' + imc + ' Obez';
+		color = 'red';
+	}
+
+	var paragraf = document.getElementById('output');
+	paragraf.innerHTML = mesajDeOutput;
+	paragraf.style.backgroundColor = color;
+	paragraf.style.color = 'white';
+	paragraf.style.borderRadius = '5px';
+	paragraf.style.padding = '10px';
+}
